9af1f95c46eebfaa6f0fd070ac4d9cb0e4c0e56d
sharon19024 <sharon19024@gmail.com>
2023-09-11 18:17:35 -0600

    Updated

--- DetectarTeclaUp.py
+++ DetectarTeclaUp.py
@@ -3,19 +3,16 @@
 
 # Escribir un programa que corra un bucle infinito leyendo e imprimiento las teclas y sólo terminará cuando se presione la tecla ↑ indicada como UP
 
-import readchar
+from readchar import readkey, key
 
-# Función para verificar si se ha presionado la tecla de flecha arriba (↑)
 def check_for_up_key():
+    # Función para verificar si se ha presionado la tecla de flecha arriba (↑)
     while True:
-        char = readchar.readkey()
-        if ord(char[0]) == 27:  # Código ASCII para el carácter de escape (ESC)
-            char = readchar.readkey()
-            if ord(char[0]) == 91:  # Código ASCII para '['
-                char = readchar.readkey()
-                if ord(char[0]) == 65:  # Código ASCII para 'A'
-                    print("Se presionó la tecla de flecha arriba (↑). Saliendo del bucle.")
-                    break
+        k = readkey()
+        if k == key.UP:  
+            print("Se presionó la tecla ↑. Saliendo del bucle.")
+            break
 
 # Iniciar el bucle para verificar la tecla de flecha arriba
 check_for_up_key()
+
